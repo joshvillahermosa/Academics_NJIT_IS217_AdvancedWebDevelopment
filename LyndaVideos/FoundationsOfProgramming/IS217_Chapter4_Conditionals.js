00cf9be6c47661f115e8e1b80bd3fb95dc305cba
Josh V <joshvillahermosa@gmail.com>
2013-02-02 16:45:06 -0500

    Added Individual projects

--- LyndaVideos/FoundationsOfProgramming/IS217_Chapter4_Conditionals.js
+++ LyndaVideos/FoundationsOfProgramming/IS217_Chapter4_Conditionals.js
@@ -8,6 +8,7 @@
 
 //If statement
 
+/*
 var a = 1;
 var b = 2;
 
@@ -17,4 +18,26 @@ if (a == b){
 
 if (b > a){
 	alert("b is > than a");
+}
+*/
+
+var Balance = 1000;
+
+if (Balance >= 0){
+	alert("Balance is positive");
+}
+
+else{
+	alert("Balance is negative");
+}
+
+//Remember, when testing conditions in a if statement use the == sign. The = is assignment and === strict equality operator. Example
+
+var a = 123;
+var b = "123";
+
+if (a === b){
+	alert("True");
+} else {
+	alert("False");
 }
